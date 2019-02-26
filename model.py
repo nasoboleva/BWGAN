a6860702a1aa02e43ef7ce66507a839d0caa7263
Pavel Khrushkov <bazpasha@gmail.com>
2019-02-26 19:41:29 +0300

    Slight changes

--- model.py
+++ model.py
@@ -2,19 +2,27 @@ import torch
 import torch.nn as nn
 
 
+class Upsample(nn.Module):
+    def __init__(self, scale):
+        self.scale = scale
+
+    def forward(self, x):
+        row = torch.cat([x] * self.scale, dim=-1)
+        return torch.cat([row] * self.scale, dim=-2)
+
+
 class ResBlock(nn.Module):
     def __init__(self, in_channels=128, out_channels=128, kernel_size=3, sampling="no", normalize=False):
         super(ResBlock, self).__init__()
 
         same_padding = kernel_size // 2
-        kernel_size = (kernel_size, kernel_size)
 
         skip = []
         modified = []
 
         skip.append(nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1)))
         if sampling == "upsampling":
-            upsampler = nn.Upsample(scale=2)
+            upsampler = Upsample(scale=2)
             skip.append(upsampler)
             modified.append(upsampler)
         elif sampling == "downsampling":
@@ -24,16 +32,15 @@ class ResBlock(nn.Module):
         else:
             assert sampling == "no", "sampling must be in {no, upsampling, downsampling}"
 
-        modify_block = []
-        modify_block.append(nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=same_padding))
-        if normalize:
-            modify_block.append(nn.BatchNorm2d(out_channels, momentum=0.9))
-        modify_block.append(nn.ReLU())
-        modify_block.append(nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=same_padding))
-        if normalize:
-            modify_block.append(nn.BatchNorm2d(out_channels, momentum=0.9))
-        modify_block.append(nn.ReLU())
-        modified = modified + modify_block + modify_block
+        for _ in range(2):
+            modified.append(nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=same_padding))
+            if normalize:
+                modified.append(nn.BatchNorm2d(out_channels, momentum=0.9))
+            modified.append(nn.ReLU())
+            modified.append(nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=same_padding))
+            if normalize:
+                modified.append(nn.BatchNorm2d(out_channels, momentum=0.9))
+            modified.append(nn.ReLU())
 
         self.skip = nn.Sequential(*skip)
         self.modified = nn.Sequential(*modified)
@@ -46,35 +53,37 @@ class Generator(nn.Module):
     def __init__(self, noise_size=128):
         super(Generator, self).__init__()
 
+        self.noise_size = noise_size
         self.linear = nn.Linear(noise_size, noise_size * 4 * 4)
         self.generator = nn.Sequential(
-            ResBlock(sampling="upsampling", normalize=True),
-            ResBlock(sampling="upsampling", normalize=True),
-            ResBlock(sampling="upsampling", normalize=True),
-            nn.Conv2d(128, 3, kernel_size=(1, 1)),
+            ResBlock(sampling="upsampling", normalize=True, in_channels=noise_size, out_channels=noise_size),
+            ResBlock(sampling="upsampling", normalize=True, in_channels=noise_size, out_channels=noise_size),
+            ResBlock(sampling="upsampling", normalize=True, in_channels=noise_size, out_channels=noise_size),
+            nn.Conv2d(noise_size, 3, kernel_size=1),
             nn.Tanh(),
         )
 
     def forward(self, x):
-        image = self.linear(x).view((-1, 128, 4, 4))
+        image = self.linear(x).view((-1, self.noise_size, 4, 4))
         return self.generator(image)
 
 
 class Discriminator(nn.Module):
-    def __init__(self):
+    def __init__(self, noise_size):
         super(Discriminator, self).__init__()
 
+        self.noise_size = noise_size
         self.discriminator = nn.Sequential(
-            ResBlock(in_channels=4, sampling="downsampling"),
-            ResBlock(sampling="downsampling"),
-            ResBlock(),
-            ResBlock(),
+            ResBlock(sampling="downsampling", in_channels=3, out_channels=noise_size, normalize=False),
+            ResBlock(sampling="downsampling", in_channels=noise_size, out_channels=noise_size, normalize=False),
+            ResBlock(sampling="no", in_channels=noise_size, out_channels=noise_size, normalize=False),
+            ResBlock(sampling="no", in_channels=noise_size, out_channels=noise_size, normalize=False),
         )
 
-        self.linear = nn.Linear(128, 1)
+        self.linear = nn.Linear(noise_size, 1)
 
     def forward(self, x):
-        batch_size = x.shape[0]
-        discriminated = self.discriminator(x).view(batch_size, 128, -1)
+        batch_size = x.size(0)
+        discriminated = self.discriminator(x).view((batch_size, self.noise_size, -1))
         mean_discriminated = torch.mean(discriminated, dim=-1)
         return self.linear(mean_discriminated)
